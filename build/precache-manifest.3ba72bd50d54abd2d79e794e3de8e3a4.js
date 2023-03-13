9d951b6b497031b158e3428ed3c15a31ddc435b2
ariffsulaiman <105694846+ariffsulaiman@users.noreply.github.com>
2023-03-13 15:25:52 +0800

    Add files via upload

---
+++ build/precache-manifest.3ba72bd50d54abd2d79e794e3de8e3a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2beaf1f22ba2542879f9aa2ff96047dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d931c63f47251f68ff",
+    "url": "/static/js/2.260104d4.chunk.js"
+  },
+  {
+    "revision": "38a83139fa943c43d60b",
+    "url": "/static/js/main.fd787fee.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
